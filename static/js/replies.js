925ef348e33392b6b30c15bf1ba5a370a220af03
jbhouse <jbmoorhouse1@gmail.com>
2017-09-23 20:05:21 -0500

    replies are now super dry. also added app.js file to solve csrf issus

--- static/js/replies.js
+++ static/js/replies.js
@@ -1,1 +1,35 @@
+$('div.replies-list').on('click','a',function(e){
+  e.preventDefault();
+  var that = $(this)
+  var replyId = that.attr('id').slice(6,that.attr('id').length)
+  deleteReply();
+  function deleteReply(){
+    $.ajax({
+      url : "/replies/delete/"+replyId,
+      type : "DELETE",
+      data : {replyId:replyId, replyto:replyObject},
+      success : function(json){
+        $('#reply-'+json.id).parent().parent().addClass('hidden')
+      }
+    })
+  }
+})
 
+$('.reply-form').on('submit',function(e){
+  e.preventDefault();
+  var that = $(this)
+  var reply = $('#reply').val()
+  var replyId = document.getElementById('reply-button').getAttribute('name').slice(11,document.getElementById('reply-button').getAttribute('name').length)
+  makeReply();
+  function makeReply(){
+    $.ajax({
+      url       : "/replies/create/",
+      type      : "POST",
+      data      : {reply:reply, replyId:replyId, replyto:replyObject},
+      success : function(json){
+        $('div.replies-list').prepend("<div><h4>"+json.text+"</h4><p><a class='delete' id='reply-"+json.id+"' href='#'><span class='glyphicon glyphicon-remove text-danger'></span><span class='text-danger icon-label'>Delete</span></a></p></div>")
+        document.getElementById('reply').value = ''
+      }
+    })
+  }
+})
